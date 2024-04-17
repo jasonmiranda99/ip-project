3471727501d9761ffe780d8fc515c07d6626d383
jasonmiranda99 <jasonmiranda999@gmail.com>
2024-04-17 10:57:13 +0530

    minor changes

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ cap.set(4,height)
 
 
 #getimages
-pathImages=os.listdir(folderPath)
-
+pathImages=sorted(os.listdir(folderPath),key=len)
 print (pathImages)
+
 
 
 while True:
     success, img=cap.read()
+    pathFullImage=os.path.join(folderPath,pathImages[0])
+
+
     cv2.imshow("Image",img)
     key=cv2.waitKey(1)
 
