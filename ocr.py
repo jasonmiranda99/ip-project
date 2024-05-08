2746d1f71f2d352ec747578808b37cccc722756d
jasonmiranda99 <jasonmiranda999@gmail.com>
2024-05-08 21:24:02 +0530

    delted ocr.py

--- ocr.py
+++
@@ -1,7 +0,0 @@
-import easyocr
-
-reader = easyocr.Reader(['en']) # specify the language
-result = reader.readtext('image.jpg')
-
-for (bbox, text, prob) in result:d
-    print(f'Text: {text}, Probability: {prob}')
